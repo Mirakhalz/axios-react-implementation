20637139ce35e0e4701a37bf01132aff3a4e9174
Aisha <mirakhalz@gmail.com>
2023-04-06 12:03:36 -0600

    First commit

---
+++ src/MyComponent.js
@@ -0,0 +1,19 @@
+import React from ,{useState,useEffect} from 'react';
+import axios from 'axios';
+
+const MyComponent = () => {
+
+    //state variable hooks
+
+    const [data,setData]=useState([])
+
+    //function declaration
+
+    return (
+        <div>
+            {/*here the JSX syntax goes here */}
+        </div>
+    )
+}
+
+export default
